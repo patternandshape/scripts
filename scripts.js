1f25ff2f828f849fff28e57f3dd25a6bca7da195
Alissa Paluska and Molly Curtin <patternandshape@gmail.com>
2016-01-13 15:04:46 -0800

    add scripts file

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+//Array Practice
+  // var sibling = ["harry","karen"];
+  // var parents = ["patricia","kevin"];
+  // console.log(sibling.concat(parents);
+  // sibling.push("lord summerisle");
+  // console.log(sibling);
+  // sibling.reverse();
+  // console.log(sibling);
+  // parents[0,1];
+  // console.log(parents);
+  // parents[1]='bob';
+  // console.log(parents);
+
+
+});
